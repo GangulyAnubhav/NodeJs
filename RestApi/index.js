94ff2f2d0096d46caca2bcfbaaf870114011bd22
Anubhav Ganguly <anubhav_g@pursuitsoftware.com>
2025-11-20 17:40:41 +0530

    Pushing my changes to dev branch

--- RestApi/index.js
+++ RestApi/index.js
@@ -3,14 +3,13 @@ const app = express();
 const usersRouter = require('./routes/users');
 const port = 3000;
 
-//Get All Users.
+//Without express.json(), Express receives the body as raw, unreadable bytes, and req.body will be undefined
+app.use(express.json());
+
 app.use('/users', usersRouter);
 
 //Get A Particular User.
 app.use('/users/:id',usersRouter);
-
-//Add A User
-app.use('/users',usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
