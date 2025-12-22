9f3d04dbdc20442a48d9159bee9e59d9a75b8775
Anubhav Ganguly <anubhav_g@pursuitsoftware.com>
2025-12-22 19:38:37 +0530

    Pushing changes to dev branch

--- JWT_Authentication/app.js
+++ JWT_Authentication/app.js
@@ -3,13 +3,13 @@ const express = require('express')
 const cookieParser =  require('cookie-parser')
 const app = express()
 const port = process.env.PORT
-const usersRoute = require('./routes/authRoutes')
+const authRoute = require('./routes/authRoutes')
 
 //Without express.json(), Express receives the body as raw, unreadable bytes, and req.body will be undefined
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/',usersRoute)
+app.use('/', authRoute)
 
 app.listen(port,() =>{
     console.log(`Server is running on http://localhost:${port}`)
